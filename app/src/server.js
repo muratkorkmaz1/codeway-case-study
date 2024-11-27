055c9ee57cea2b929995ab0c1ea3fe5ec0031cda
Murat Korkmaz <muratkorkmaz@muratkorkmaz.local>
2024-11-27 23:02:54 +0300

    second commit

--- app/src/server.js
+++ app/src/server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-require("./app/routes/tutorial.routes.js")(app);
+require("../routes/tutorial.routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
